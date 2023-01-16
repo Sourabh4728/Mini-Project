122062e3b17d49a29a50f5daad77741a063261df
unknown <rohinitibile25@gmail.com>
2023-01-16 22:30:55 +0530

    Student Score sucefully updated

---
+++ StudentTable.sql
@@ -0,0 +1,8 @@
+create table Student(
+ID int auto_increment primary key,
+StudentName varchar(255),
+Score varchar(255)
+);
+
+insert into Student(StudentName,Score) values('Ram Patil','');
+insert into student(StudentName,Score)values('Ashish Patil','');
